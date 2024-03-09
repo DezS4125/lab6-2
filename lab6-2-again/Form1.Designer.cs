79f3b45b19311790166e0bdabbcb7cb6b7311d6b
User <User@WINDEV2311EVAL>
2024-03-08 22:58:17 -0800

    Add project files.

---
+++ lab6-2-again/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace lab6_2_again
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.qlcbDataSet = new lab6_2_again.qlcbDataSet();
+            this.chucvuBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.chucvuTableAdapter = new lab6_2_again.qlcbDataSetTableAdapters.chucvuTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.qlcbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chucvuBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.chucvuBindingSource;
+            this.comboBox1.DisplayMember = "tenCV";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(187, 204);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.ValueMember = "maCV";
+            // 
+            // qlcbDataSet
+            // 
+            this.qlcbDataSet.DataSetName = "qlcbDataSet";
+            this.qlcbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // chucvuBindingSource
+            // 
+            this.chucvuBindingSource.DataMember = "chucvu";
+            this.chucvuBindingSource.DataSource = this.qlcbDataSet;
+            // 
+            // chucvuTableAdapter
+            // 
+            this.chucvuTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 461);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.qlcbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chucvuBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private qlcbDataSet qlcbDataSet;
+        private System.Windows.Forms.BindingSource chucvuBindingSource;
+        private qlcbDataSetTableAdapters.chucvuTableAdapter chucvuTableAdapter;
+    }
+}
+
